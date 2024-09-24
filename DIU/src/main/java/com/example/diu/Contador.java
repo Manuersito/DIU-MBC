1b3efc832bb74fce2a272480a034d5e6c0338efe
Manuersito <manuelbc03@hotmail.com>
2024-09-24 14:15:29 +0200

    ACT 24-09-24

--- DIU/src/main/java/com/example/diu/Contador.java
+++ DIU/src/main/java/com/example/diu/Contador.java
@@ -1,6 +1,7 @@
 package com.example.diu;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -13,37 +14,57 @@ import javafx.geometry.Insets;
 
 
 public class Contador extends Application {
+
+    private Button btmas,btmenos,btcero;
+    private Label contadior;
+    private int num=1;
+
+    private void botonPulsado(ActionEvent e) {
+        if (e.getSource() == btmas)
+            contadior.setText(String.valueOf(++num));
+        else if (e.getSource() == btmenos)
+            contadior.setText(String.valueOf(--num));
+        else if (e.getSource() == btcero)
+            contadior.setText(String.valueOf(num = 0));
+    }
+
+
+
     @Override
     public void start(Stage stage) throws Exception {
         try{
             VBox raiz = new VBox();
-            raiz.setPadding(new Insets(0, 0,0 , 100));
-            raiz.setSpacing(30);
+            raiz.setPadding(new Insets(0, 0,0 , 0));
+            raiz.setSpacing(0);
+            raiz.getStyleClass().add("raiz");
 
-            Label contadior;
+
             contadior = new Label("1");
-            contadior.setFont(Font.font(20));
+            contadior.setFont(Font.font(60));
+            contadior.setPadding(new Insets(30,0,0,130));
+            contadior.getStyleClass().add("contadior");
 
             HBox botones = new HBox();
-            botones.setPadding(new Insets(30, 0, 0, 0));
-            botones.setSpacing(3);
+            botones.setPadding(new Insets(30, 0, 0,50 ));
+            botones.setSpacing(60);
 
-           javafx.scene.control.Button btmas;
-           javafx.scene.control.Button btmenos;
-           javafx.scene.control.Button btcero;
-
-           btmas = new javafx.scene.control.Button();
-           btmenos = new javafx.scene.control.Button();
-           btcero = new javafx.scene.control.Button();
+           btmas = new Button();
+           btmenos = new Button();
+           btcero = new Button();
 
             btmas.setText("+");
+            btmas.setOnAction(e->botonPulsado(e));
             btmenos.setText("-");
+            btmenos.setOnAction(e->botonPulsado(e));
             btcero.setText("0");
+            btcero.getStyleClass().add("btcero");
+            btcero.setOnAction(e->botonPulsado(e));
 
             botones.getChildren().addAll(btmenos, btcero, btmas);
             raiz.getChildren().addAll(botones, contadior);
 
             Scene escena = new Scene(raiz, 300, 200);
+            escena.getStylesheets().add(getClass().getResource("/CSS/Contador.css").toExternalForm());
             stage.setTitle("Contador");
             stage.setScene(escena);
             stage.show();
