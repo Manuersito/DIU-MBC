5cd5efa73920931eb1122f1486ae147a6418c469
Manuersito <manuelbc03@hotmail.com>
2025-02-12 00:06:27 +0100

    desde casa 11 feb

--- React/agenda-proyecto/src/App.jsx
+++ React/agenda-proyecto/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import NavBarComponent from './components/NavBarComponent.jsx';
+import ContactList from './components/ContactList.jsx';
 
 export default function App() {
 
   return (
     <>
      <NavBarComponent />
-
+<ContactList />
     </>
   )
 }
